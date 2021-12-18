04b114e075db0ebcf8a90df9bc48be7549784ddc
gfbatista <gilbertoferrari@gmail.com>
2021-12-18 12:28:42 -0300

    chore: initial commit

---
+++ src/main/kotlin/com/luizalabs/zipcode/ZipcodeApplication.kt
@@ -0,0 +1,11 @@
+package com.luizalabs.zipcode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZipcodeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ZipcodeApplication>(*args)
+}
