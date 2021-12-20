0fa87de9a9735f854b5df95adc5e9c0f8ecb846f
gfbatista <gilbertoferrari@gmail.com>
2021-12-20 11:29:30 -0300

    chore: add swagger config

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.flywaydb:flyway-core:7.15.0")
 	implementation("org.springframework.boot:spring-boot-starter-parent:2.4.0")
+	implementation("io.springfox:springfox-swagger2:3.0.0")
+	implementation("io.springfox:springfox-boot-starter:3.0.0")
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
 
 	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
