4bb677371b903518bf110fde1df5febc92b12342
gfbatista <gilbertoferrari@gmail.com>
2021-12-21 14:36:29 -0300

    chore: change zipcode get response

--- src/main/kotlin/com/luizalabs/zipcode/controller/response/ZipCodeResponse.kt
+++ src/main/kotlin/com/luizalabs/zipcode/controller/response/ZipCodeResponse.kt
@@ -1,9 +1,22 @@
 package com.luizalabs.zipcode.controller.response
 
 data class ZipCodeResponse(
-    var code: String,
     var street: String,
     var district: String,
     var city: String,
     var state: String
-)
+) {
+    var code: String = ""
+
+    constructor(
+        code: String,
+        street: String,
+        district: String,
+        city: String,
+        state: String
+    ) : this(street, district, city, state) {
+        val prefix: String = code.substring(0, code.length - 3)
+        val suffix: String = code.substring(code.length - 3, code.length)
+        this.code = prefix.plus("-").plus(suffix)
+    }
+}
